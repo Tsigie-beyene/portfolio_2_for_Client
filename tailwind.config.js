99b30d94b254a5a919487c3effd232ee8d6c4722
Tsigie-beyene <tsigiebey29@gmail.com>
2025-07-24 19:49:02 +0300

    correct atilwind config to add customized fonts

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F',
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}; 
