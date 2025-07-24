0f52c03f3c76652afb3234653d40955d38157f68
Tsigie-beyene <tsigiebey29@gmail.com>
2025-07-24 15:20:08 +0300

    contact me section

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,10 +14,23 @@ const Header = () => {
         </h3>
         <h1 className='text-3xl lg:text-4xl xl:text-[36px] font-Ovo'>
         Robotics & Control Master’s Student | Human-Robot Interaction | Mechatronics Engineer        </h1>
-        <p className='max-w-2xl mx-auto font-Ovo text-gray-500'>
-        I’m Seid Fikreselassie, a Master’s student in Automatic Control and Robotics, with a strong background in Mechatronics and hands-on experience in human-robot interaction and intelligent control. I’m passionate about building smart, autonomous, and human-centered robotic systems.        </p>
+        <p className='max-w-3xl mx-auto font-Ovo text-gray-500'>
+        I’m Seid Fikreselassie, a Master’s student in Automatic Control and Robotics, with a strong background in Mechatronics and hands-on experience in human-robot interaction and intelligent control. I’m passionate about building smart, autonomous, and human-centered robotic systems. 
+        </p>
+        <div className='flex flex-col sm:flex-row gap-4'>
+          <a href="#contact"
+          className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
+          Contact Me 
+          <Image src={assets.right_arrow_white } alt="right arrow" className='w-4'/>
+          </a>
+          <a href="sample-resume.pdf" download 
+          className='px-10 py-3 border border rounded-full border-gray-500 flex items-center gap-2 hover:bg-hover:text-blue-500'>
+            My Resume <Image src={assets.download_icon} alt="Download Icocn" className='w-4'/>
+          </a>
+        </div>
     </div>
   )
+  
 }
 
 export default Header
