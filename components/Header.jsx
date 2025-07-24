d355df62c18dad901bb9d52532ba739490cae840
Tsigie-beyene <tsigiebey29@gmail.com>
2025-07-24 16:53:55 +0300

    finished the header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,15 +9,15 @@ const Header = () => {
             <Image src={assets.fikiru} alt="Profile Image" className="rounded-full w-32"/>
         </div>
         <h3 className='flex items-end gap-2 text-xl md:text-2xl font-semibold mb-3 font-Ovo'>
-            Hi! I'm Seid Fikreselassie <Image src={assets.hand_icon} alt="Hand Icon" className="w-6"/>
+            Hi! I'm Seid Fikreselassie 😊 
 
         </h3>
         <h1 className='text-3xl lg:text-4xl xl:text-[36px] font-Ovo'>
-        Robotics & Control Master’s Student | Human-Robot Interaction | Mechatronics Engineer        </h1>
+        Robotics & Control Master’s Student | Human-Robot Interaction | Mechatronics Engineer </h1>
         <p className='max-w-3xl mx-auto font-Ovo text-gray-500'>
         I’m Seid Fikreselassie, a Master’s student in Automatic Control and Robotics, with a strong background in Mechatronics and hands-on experience in human-robot interaction and intelligent control. I’m passionate about building smart, autonomous, and human-centered robotic systems. 
         </p>
-        <div className='flex flex-col sm:flex-row gap-4'>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
           <a href="#contact"
           className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>
           Contact Me 
