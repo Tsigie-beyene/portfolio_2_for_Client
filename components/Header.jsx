e6d824e3ca2d7a4332720f2719c8c09f9a42c9c0
Tsigie-beyene <tsigiebey29@gmail.com>
2025-07-25 09:06:06 +0300

    remove tailwind config

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,13 +8,13 @@ const Header = () => {
         <div>
             <Image src={assets.fikiru} alt="Profile Image" className="rounded-full w-32"/>
         </div>
-        <h3 className='flex items-end gap-2 text-xl md:text-2xl font-semibold mb-3 font-Ovo'>
+        <h3 className='flex items-end gap-2 text-xl md:text-2xl font-semibold mb-3 font-ovo'>
             Hi! I'm Seid Fikreselassie 😊 
 
         </h3>
-        <h1 className='text-2xl lg:text-4xl xl:text-[36px] font-Ovo'>
+        <h1 className='text-2xl lg:text-4xl xl:text-[36px] font-ovo'>
         Robotics & Control Master’s Student | Human-Robot Interaction | Mechatronics Engineer </h1>
-        <p className='max-w-3xl mx-auto font-Ovo text-gray-500'>
+        <p className='max-w-3xl mx-auto font-ovo text-gray-500'>
         I’m Seid Fikreselassie, a Master’s student in Automatic Control and Robotics, with a strong background in Mechatronics and hands-on experience in human-robot interaction and intelligent control. I’m passionate about building smart, autonomous, and human-centered robotic systems. 
         </p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
@@ -24,7 +24,7 @@ const Header = () => {
           <Image src={assets.right_arrow_white } alt="right arrow" className='w-4'/>
           </a>
           <a href="sample-resume.pdf" download 
-          className='px-10 py-3 border border rounded-full border-gray-500 flex items-center gap-2 hover:bg-hover:text-blue-500'>
+          className='px-10 py-3 border border rounded-full border-gray-500 flex items-center gap-2 hover:bg-light-hover hover:text-blue-500'>
             My Resume <Image src={assets.download_icon} alt="Download Icocn" className='w-4'/>
           </a>
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
         <button>
             <Image src={assets.moon_icon} alt='moon icon' className='w-6'/>
         </button>
-        <a href="#contact" className='hidden md:flex items-center gap-3 px-10 py-2.5 border lg:text-base xl:text-lg font-semibold text-gray-700 rounded-full hover:bg-borf'>
+        <a href="#contact" className='hidden md:flex items-center gap-3 px-10 py-2.5 border lg:text-base xl:text-lg font-semibold text-gray-700 rounded-full hover:bg-light-hover'>
 Contact <Image src={assets.arrow_icon} alt="Arrow Icon" className="w-3"/></a>
      <button className='block md:hidden ml-3' onClick={openMenu}>
             <Image src={assets.menu_black} alt='moon icon' className='w-6 hover:text-white-500'/>
